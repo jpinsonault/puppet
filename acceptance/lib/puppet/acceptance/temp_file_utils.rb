jpinsonault/puppet

commit 8d2a8722642cb66f54bf4ee2e865125f32581581

(PUP-4688) Define helper methods for stat'ing files
Previously, the acceptance tests executed `stat --format '%U:%G %a`
which doesn't work on osx/bsd. Instead we need to use `stat -f
'%Su:%Sg %p`. The capital `S` ensures we get back names and not
ids. Also the `%p` format specifier returns the raw mode, which
includes the upper file type bit fields, e.g. directory, file,
fifo. That is unlike Linux where `%a` returns only the access rights
in octal.

This commit adds a `stat` helper method that returns a permissions
triplet of owner, group, mode. The latter is returned as an integer
and only contains the access rights, e.g. 0750.

Also, several tests assumed the primary group for the root user was
named "root" though for osx/bsd it's "wheel", etc. This commit adds
`root_user` and `root_group` to account for those difference, and
updates the tests to call it.

acceptance/lib/puppet/acceptance/temp_file_utils.rb
@@ -2,6 +2,30 @@ module Puppet
   module Acceptance
     module TempFileUtils
 
+      # Return the name of the root user, as appropriate for the platform.
+      def root_user(host)
+        case host['platform']
+        when /windows/
+          'Administrator'
+        else
+          'root'
+        end
+      end
+
+      # Return the name of the root group, as appropriate for the platform.
+      def root_group(host)
+        case host['platform']
+        when /windows/
+          'Administrators'
+        when /aix/
+          'system'
+        when /osx|bsd/
+          'wheel'
+        else
+          'root'
+        end
+      end
+
       # Create a file on the host.
       # Parameters:
       # [host] the host to create the file on
@@ -22,28 +46,14 @@ module Puppet
           if host['roles'].include?('master') then
             options[:owner] = host.puppet['user']
           else
-            case host['platform']
-            when /windows/
-              options[:owner] = 'Administrator'
-            else
-              options[:owner] = 'root'
-            end
+            options[:owner] = root_user(host)
           end
         end
         unless options[:group]
           if host['roles'].include?('master') then
             options[:group] = host.puppet['group']
           else
-            case host['platform']
-            when /windows/
-              options[:group] = 'Administrators'
-            when /aix/
-              options[:group] = 'system'
-            when /osx|bsd/
-              options[:group] = 'wheel'
-            else
-              options[:owner] = 'root'
-            end
+            options[:group] = root_group(host)
           end
         end
 
@@ -125,6 +135,23 @@ module Puppet
         on(host, "chmod #{mode} #{path}")
       end
 
+      # Returns an array containing the owner, group and mode of
+      # the file specified by path. The returned mode is an integer
+      # value containing only the file mode, excluding the type, e.g
+      # S_IFDIR 0040000
+      def stat(host, path)
+        stat_command = case agent['platform']
+                       when /osx/
+                         "stat -f '%Su:%Sg:%p'"
+                       else
+                         "stat --format '%U:%G:%a'"
+                       end
+
+        permissions = on(host, "#{stat_command} #{path}").stdout.chomp
+        owner, group, mode = permissions.split(':')
+
+        [owner, group, mode.to_i(8) & 07777]
+      end
 
       def initialize_temp_dirs()
         # pluck this out of the test case environment; not sure if there is a better way

acceptance/tests/config/apply_file_metadata_specified_in_config.rb
@@ -16,11 +16,24 @@ agents.each do |agent|
   }
   SITE
 
-  on(agent, puppet('config', 'set', 'logdir', "'#{logdir} { owner = root, group = root, mode = 0700 }'", '--confdir', get_test_file_path(agent, '')))
+  user = root_user(agent)
+  group = root_group(agent)
+
+  # puppet only always the group to be 'root' or 'service', but not
+  # all platforms have a 'root' group, e.g. osx.
+  permissions =
+    if group == 'root'
+      "{ owner = #{user}, group = root, mode = 0700 }"
+    else
+      "{ owner = #{user}, mode = 0700 }"
+    end
+
+  on(agent, puppet('config', 'set', 'logdir', "'#{logdir} #{permissions}'", '--confdir', get_test_file_path(agent, '')))
 
   on(agent, puppet('apply', get_test_file_path(agent, 'site.pp'), '--confdir', get_test_file_path(agent, '')))
 
-  on(agent, "stat --format '%U:%G %a' #{logdir}") do
-    assert_match(/root:root 700/, stdout)
-  end
+  permissions = stat(agent, logdir)
+  assert_equal(user, permissions[0], "File owner #{permissions[0]} does not match expected user #{user}")
+  assert_equal(group, permissions[1], "File group #{permissions[1]} does not match expected group #{group}")
+  assert_equal(0700, permissions[2], "File mode #{permissions[2].to_s(8)} does not match expected mode 0700")
 end

acceptance/tests/resource/file/symbolic_modes.rb
@@ -1,5 +1,8 @@
 test_name "file resource: symbolic modes"
 
+require 'puppet/acceptance/temp_file_utils'
+extend Puppet::Acceptance::TempFileUtils
+
 module FileModeAssertions
   include Beaker::DSL::Assertions
 
@@ -12,8 +15,8 @@ module FileModeAssertions
   end
 
   def assert_mode(agent, path, expected_mode)
-    current_mode = testcase.on(agent, "stat --format '%a' #{path}").stdout.chomp.to_i(8)
-    assert_equal(expected_mode, current_mode, "current mode #{current_mode.to_s(8)} doesn't match expected mode #{expected_mode.to_s(8)}")
+    permissions = testcase.stat(agent, path)
+    assert_equal(expected_mode, permissions[2], "current mode #{permissions[2].to_s(8)} doesn't match expected mode #{expected_mode.to_s(8)}")
   end
 
   def assert_mode_change(agent, manifest, path, symbolic_mode, start_mode, expected_mode)

acceptance/tests/ticket_9862_puppet_runs_without_service_user_or_group_present.rb
@@ -8,9 +8,9 @@ extend Puppet::Acceptance::TempFileUtils
 initialize_temp_dirs
 
 def assert_ownership(agent, location, expected_user, expected_group)
-  on(agent, "stat --format '%U:%G' #{location}") do
-    assert_match(/#{expected_user}:#{expected_group}/, stdout)
-  end
+  permissions = stat(agent, location)
+  assert_equal(expected_user, permissions[0], "Owner #{permissions[0]} does not match expected #{expected_user}")
+  assert_equal(expected_group, permissions[1], "Group #{permissions[1]} does not match expected #{expected_group}")
 end
 
 def missing_directory_for(agent, dir)
@@ -42,7 +42,7 @@ agents.each do |agent|
                    '--group', 'missinggroup') do
 
     assert_match(/puppet_run/, stdout)
-    assert_ownership(agent, logdir, 'root', 'root')
+    assert_ownership(agent, logdir, root_user(agent), root_group(agent))
   end
 end
 

commit 297a43c18b3104c6c041bef4c848376a8e8dbb08

(maint) Fix bug in stat function
This is coming from a CI failure for puppet-server, which uses puppet
4.2.1, see SERVER-832 for details.

The stat function references an agent variable, which will coincidentally
be set correctly if the beaker config only has one host with an agent role.
But if there is more than one agent, the 'agent' variable will be a list
of hosts, and the stat function will throw an exception.

The bug was introduced in 8d2a872

acceptance/lib/puppet/acceptance/temp_file_utils.rb
@@ -140,7 +140,7 @@ module Puppet
       # value containing only the file mode, excluding the type, e.g
       # S_IFDIR 0040000
       def stat(host, path)
-        stat_command = case agent['platform']
+        stat_command = case host['platform']
                        when /osx/
                          "stat -f '%Su:%Sg:%p'"
                        else